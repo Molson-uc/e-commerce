f84b193ff4864c42ee87b492ebc0e5e6f7ec8bf4
MichalMolik <molson.and@gmail.com>
2024-01-19 17:55:53 +0100

    add thumbnails products

--- backend/ecommerce/api/models.py
+++ backend/ecommerce/api/models.py
@@ -1,10 +1,14 @@
 from django.urls import reverse
 from django.db import models
+from stdimage import StdImageField
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Product(models.Model):
@@ -13,7 +17,12 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    image = StdImageField(upload_to="products/%Y/%m/%d", blank=False)
+    thumbnail = StdImageField(
+        upload_to="products/%Y/%m/%d",
+        variations={"thumbnail": {"width": 100, "height": 75}},
+        blank=False,
+    )
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
