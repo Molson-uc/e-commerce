8996112b04a995b7d8e382f4901e4b53483d27b9
MichalMolik <molson.and@gmail.com>
2024-01-20 16:11:01 +0100

    auto thumbnail

--- backend/ecommerce/api/models.py
+++ backend/ecommerce/api/models.py
@@ -1,6 +1,12 @@
+from collections.abc import Iterable
 from django.urls import reverse
 from django.db import models
-from stdimage import StdImageField
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
 class Category(models.Model):
@@ -17,11 +23,9 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = StdImageField(upload_to="products/%Y/%m/%d", blank=False)
-    thumbnail = StdImageField(
-        upload_to="products/%Y/%m/%d",
-        variations={"thumbnail": {"width": 100, "height": 75}},
-        blank=False,
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=False)
+    thumbnail = models.ImageField(
+        upload_to="products/tb/%Y/%m/%d", blank=False, editable=False
     )
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -29,5 +33,21 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return self.name
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.create_thumbnail()
+
+    def create_thumbnail(self):
+        if self.image:
+            img = Image.open(self.image.path)
+            thumbnail_size = (200, 200)
+            img.thumbnail(thumbnail_size)
+            thumbnail_io = BytesIO()
+            filename, extension = os.path.splitext(os.path.basename(self.image.name))
+            img.save(
+                thumbnail_io, format="JPEG" if extension.lower() == "jpg" else "PNG"
+            )
+            thumbnail_name = f"thumbnail_{filename}{extension.lower()}"
+            thumbnail_file = SimpleUploadedFile(thumbnail_name, thumbnail_io.getvalue())
+            self.thumbnail.save(thumbnail_file.name, thumbnail_file, save=False)
+            super().save()
--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "api",
+    "imagekit",
 ]
 
 
@@ -46,6 +49,8 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
